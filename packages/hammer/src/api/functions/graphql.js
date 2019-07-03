0d438e432aafa7bc489fe375423cadbe0c98dc10
Peter Pistorius <peter.pistorius@gmail.com>
2019-07-03 21:59:24 +0200

    Move GraphQL serverless function into hammer package.

---
+++ packages/hammer/src/api/functions/graphql.js
@@ -0,0 +1,49 @@
+require("@babel/register")({
+  // Find babel.config.js up the folder structure.
+  rootMode: "upward",
+  // Since babel ignores all files outside the cwd, it does not compile sibling packages
+  // So rewrite the ignore list to only include node_modules
+  ignore: ["node_modules"]
+});
+import path from "path";
+import requireDir from "require-dir";
+import { queryType, makeSchema } from "nexus";
+import { ApolloServer } from "apollo-server-lambda";
+
+import { getHammerBaseDir } from "src/core";
+
+const HAMMER_BASE_DIR = getHammerBaseDir();
+
+const GRAPHQL_DIR = path.join(HAMMER_BASE_DIR, "api/src/graphql");
+const OUTPUTS_DIR = path.join(HAMMER_BASE_DIR, "api/src/generated");
+const GRAPHQL_HOWTO = "https://hammerframework.com/";
+
+const BaseQueryType = queryType({
+  definition(t) {
+    t.string("help", {
+      resolve() {
+        return `Start adding your Nexus schema definitions in ${GRAPHQL_DIR}, read more over here: ${GRAPHQL_HOWTO}`;
+      }
+    });
+  }
+});
+const moreGraphQLTypes = requireDir(GRAPHQL_DIR, {
+  recurse: false,
+  extensions: [".js"]
+});
+const schema = makeSchema({
+  types: [BaseQueryType, ...Object.values(moreGraphQLTypes)],
+  outputs: {
+    schema: path.join(OUTPUTS_DIR, "api-schema.graphql"),
+    typegen: path.join(OUTPUTS_DIR, "generated-types.d.ts")
+  }
+});
+
+export default ({ context } = {}) => {
+  const server = new ApolloServer({
+    schema,
+    context
+  });
+
+  return server.createHandler();
+};
--- packages/hammer/src/index.js
+++ packages/hammer/src/index.js
@@ -1,1 +1,9 @@
+import { default as graphql } from "src/api/functions/graphql";
+
+export const api = {
+  functions: {
+    graphql
+  }
+};
+
 export { getHammerConfigPath, getHammerBaseDir, getHammerConfig } from "./core";
