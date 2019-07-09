44465180572eb87695c84d650d1d9a0eb2d3b590
Peter Pistorius <peter.pistorius@gmail.com>
2019-07-09 10:17:29 +0200

    Get router working.

--- web/src/index.js
+++ web/src/index.js
@@ -2,15 +2,34 @@ import ReactDOM from "react-dom";
 import { GraphQLProvider } from "@hammerframework/hammer-web";
 import { ThemeProvider } from "styled-components";
 
+import { Auth0Provider } from "src/lib/auth0";
+
 import theme from "src/lib/theme";
 import Router from "src/pages/Router";
 
 import "./index.css";
 
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
 ReactDOM.render(
   <GraphQLProvider>
     <ThemeProvider theme={theme}>
-      <Router />
+      <Auth0Provider
+        domain="p4p8.eu.auth0.com"
+        client_id="b7vN4sVz6yjGrq82ctXJW9NRTvlWzkFU"
+        redirect_uri={window.location.origin}
+        onRedirectCallback={onRedirectCallback}
+      >
+        <Router />
+      </Auth0Provider>
     </ThemeProvider>
   </GraphQLProvider>,
   document.getElementById("hammer-app")
--- web/src/pages/Router.js
+++ web/src/pages/Router.js
@@ -1,13 +1,17 @@
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { SecureRoute } from "src/lib/auth0";
 
 import InvoicePage from "./InvoicePage";
+import Profile from "./Profile";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <>
+      <Switch>
         <Route path="/" exact component={InvoicePage} />
-      </>
+        <SecureRoute path="/profile" component={Profile} />
+      </Switch>
     </BrowserRouter>
   );
 };
