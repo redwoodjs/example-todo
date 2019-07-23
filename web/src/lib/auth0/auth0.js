72ba3a1988e4e9052db6e5a833479d673e87657c
Peter Pistorius <peter.pistorius@gmail.com>
2019-07-23 09:51:45 +0200

    Pass token through graphql query headers.

--- web/src/lib/auth0/auth0.js
+++ web/src/lib/auth0/auth0.js
@@ -1,2 +1,22 @@
+import { GraphQLProvider, createClient } from "@hammerframework/hammer-web";
+
 export { Auth0Provider, useAuth0 } from "./Auth0Provider";
 export { default as SecureRoute } from "./SecureRoute";
+
+export const GraphQLWithAuth0Provider = ({ audience, ...rest }) => {
+  return (
+    <GraphQLProvider
+      client={createClient({
+        fetchOptions: async () => {
+          const token = await useAuth0().getTokenSilently({ audience });
+          return {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          };
+        }
+      })}
+      {...rest}
+    />
+  );
+};
