5f0c18763f89d6427c12f15d7feef9a94fa49dd3
Peter Pistorius <peter.pistorius@gmail.com>
2019-07-27 18:55:40 +0200

    Reorder some imports.

--- packages/hammer-web/src/index.js
+++ packages/hammer-web/src/index.js
@@ -1,5 +1,5 @@
-export { gql } from "graphql-tag";
-
 export { default as HammerProvider } from "./HammerProvider";
+
+export { gql } from "graphql-tag";
 export { default as Query } from "./graphql/Query";
 export { GraphQLProvider, createGraphQLClient } from "./graphql";
