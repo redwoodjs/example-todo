ddadf472c79e7988d75e949ba1a42e1af6e4527c
Peter Pistorius <peter.pistorius@gmail.com>
2019-08-22 11:01:00 +0200

    Re-export useQuery/mutation.

--- packages/hammer-web/src/index.js
+++ packages/hammer-web/src/index.js
@@ -3,5 +3,6 @@ export { default as HammerProvider, useAuth } from "./HammerProvider";
 export { gql } from "graphql-tag";
 export { default as Query } from "./graphql/Query";
 export { GraphQLProvider, createGraphQLClient } from "./graphql";
+export { useQuery, useMutation } from "@apollo/react-hooks";
 
 export { BrowserRouter, Switch, AnonRoute, AuthRoute } from "./Router";
