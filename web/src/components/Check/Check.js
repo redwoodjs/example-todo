63e796c75941268c893ff62a05620bdb017c68b7
Tom Preston-Werner <tom@mojombo.com>
2019-09-02 23:01:50 -0700

    SVG!

--- web/src/components/Check/Check.js
+++ web/src/components/Check/Check.js
@@ -1,25 +1,34 @@
 import styled from "styled-components";
 
+import IconOn from "./on.svg";
+import IconOff from "./off.svg";
+import IconPlus from "./plus.svg";
+
 const Check = ({ type }) => {
   if (type === "on") {
-    return <SC.CheckOn />;
+    return (
+      <SC.Icon>
+        <IconOn />
+      </SC.Icon>
+    );
   } else if (type === "off") {
-    return <SC.CheckOff />;
+    return (
+      <SC.Icon>
+        <IconOff />
+      </SC.Icon>
+    );
   } else if (type === "plus") {
-    return <SC.CheckOn />;
+    return (
+      <SC.Icon>
+        <IconPlus />
+      </SC.Icon>
+    );
   }
 };
 
 const SC = {};
-SC.CheckOff = styled.div`
-  border-radius: 100px;
-  border: 2px solid #8000ff;
-  width: 20px;
-  height: 20px;
+SC.Icon = styled.div`
   margin-right: 15px;
-`;
-SC.CheckOn = styled(SC.CheckOff)`
-  background-color: #8000ff;
 `;
 
 export default Check;
