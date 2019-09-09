e1123b3785661ddb1514d4e430a00ea2b0f880ac
Peter Pistorius <peter.pistorius@gmail.com>
2019-09-09 09:26:12 +0200

    Generate photon in generated folder.

--- api/src/lib/photon.js
+++ api/src/lib/photon.js
@@ -1,14 +1,14 @@
-import Photon from "@generated/photon";
+import Photon from '../../generated/photon'
 
-let PHOTON_INSTANCE;
+let PHOTON_INSTANCE
 
 export const getPhoton = async () => {
   if (PHOTON_INSTANCE) {
-    return PHOTON_INSTANCE;
+    return PHOTON_INSTANCE
   }
 
-  const photon = new Photon();
-  await photon.connect();
-  PHOTON_INSTANCE = photon;
-  return PHOTON_INSTANCE;
-};
+  const photon = new Photon()
+  await photon.connect()
+  PHOTON_INSTANCE = photon
+  return PHOTON_INSTANCE
+}
