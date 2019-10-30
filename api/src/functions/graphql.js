b834082ebe567fdd09a835077292280d537e1b08
Peter Pistorius <peter.pistorius@gmail.com>
2019-10-30 22:32:49 +0100

    Fix handler export.

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -1,14 +1,13 @@
-import { server } from '@hammerframework/hammer-api'
 import { Photon } from '@generated/photon'
+import { server } from '@hammerframework/hammer-api'
 
 import * as todo from 'src/graphql/todo'
 
 const photon = new Photon()
 
-export const handler = () =>
-  server({
-    context: {
-      photon,
-    },
-    types: [todo],
-  }).createHandler()
+export const handler = server({
+  context: {
+    photon,
+  },
+  types: [todo],
+}).createHandler()
