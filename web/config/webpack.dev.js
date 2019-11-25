f230c9309755e53f9f675e3693b21f609c3313af
Peter Pistorius <peter.pistorius@gmail.com>
2019-11-25 08:44:37 +0100

    Use hammer-scripts config.

--- web/config/webpack.dev.js
+++ web/config/webpack.dev.js
@@ -1,38 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const path = require('path')
 
-const merge = require('webpack-merge')
-const { getHammerConfig } = require('@hammerframework/hammer-core')
-const escapeRegExp = require('lodash.escaperegexp')
+const config = require('@hammerframework/scripts/config/webpackDevServer.config')
 
-const webpackConfig = require('./webpack.common.js')
-const WEBPACK_MODE = 'development'
-
-const hammerConfig = getHammerConfig()
-
-module.exports = merge(webpackConfig(), {
-  mode: WEBPACK_MODE,
-  devtool: 'inline-source-map',
-  devServer: {
-    hot: true,
-    writeToDisk: false,
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, '../dist'),
-    port: hammerConfig.web.port,
-    proxy: {
-      [hammerConfig.web.apiProxyPath]: {
-        target: `http://localhost:${hammerConfig.api.port}`,
-        pathRewrite: {
-          [`^${escapeRegExp(hammerConfig.web.apiProxyPath)}`]: '',
-        },
-      },
-    },
-    inline: true,
-    overlay: true,
-  },
-  optimization: {
-    removeAvailableModules: false,
-    removeEmptyChunks: false,
-    splitChunks: false,
-  },
-})
+module.exports = config
