c42ed2e667fa80c53fdc32b856cd6b2900b6fb9d
Tom Preston-Werner <tom@mojombo.com>
2019-12-04 15:27:11 -0800

    Provide pathname via Location context.

--- web/src/lib/HammerRouter.js
+++ web/src/lib/HammerRouter.js
@@ -4,12 +4,62 @@ const rePath = (path) => {
   return fullString
 }
 
-export const Router = ({ children }) => {
+const createNamedContext = (name, defaultValue) => {
+  const Ctx = React.createContext(defaultValue)
+  Ctx.displayName = name
+  return Ctx
+}
+
+const LocationContext = createNamedContext('Location')
+
+const Location = ({ children }) => (
+  <LocationContext.Consumer>
+    {(context) =>
+      context ? (
+        children(context)
+      ) : (
+        <LocationProvider>{children}</LocationProvider>
+      )
+    }
+  </LocationContext.Consumer>
+)
+
+class LocationProvider extends React.Component {
+  static defaultProps = {
+    pathname: window.location.pathname,
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      context: {
+        pathname: this.props.pathname,
+      },
+    }
+  }
+
+  render() {
+    let { children } = this.props
+    let { context } = this.state
+    return (
+      <LocationContext.Provider value={context}>
+        {typeof children === 'function' ? children(context) : children || null}
+      </LocationContext.Provider>
+    )
+  }
+}
+
+export const Router = (props) => (
+  <Location>
+    {(locationContext) => <RouterImpl {...locationContext} {...props} />}
+  </Location>
+)
+
+export const RouterImpl = ({ pathname, children }) => {
   const routes = React.Children.toArray(children)
-  const url = window.location.pathname
   for (let route of routes) {
     const { path, page: Page } = route.props
-    const matches = Array.from(url.matchAll(rePath(path)))
+    const matches = Array.from(pathname.matchAll(rePath(path)))
     if (matches.length > 0) {
       const pathProps = matches[0].groups
       return <Page {...pathProps} />
