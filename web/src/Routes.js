e4b61473f4fd35b83b545b824976ad47e30991e3
Tom Preston-Werner <tom@mojombo.com>
2019-12-18 16:53:10 -0800

    Add notfound route.

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,10 +1,13 @@
 import HomePage from 'src/pages/HomePage'
 import { Router, Route } from 'src/lib/HammerRouter'
+import NotFoundPage from 'src/pages/NotFoundPage'
 
 const Routes = () => {
   return (
     <Router>
       <Route path="/" page={HomePage} />
+
+      <Route notfound page={NotFoundPage} />
     </Router>
   )
 }
