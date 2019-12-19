db68e11a2cf69cd17c5a54b7513fdd0fcff99233
Tom Preston-Werner <tom@mojombo.com>
2019-12-18 16:45:38 -0800

    New router, NotFoundPage, FatalErrorPage.

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,12 +1,10 @@
 import HomePage from 'src/pages/HomePage'
-
 import { Router, Route } from 'src/lib/HammerRouter'
 
 const Routes = () => {
   return (
     <Router>
       <Route path="/" page={HomePage} />
-      <Route path="/todo/:id/:id2" page={HomePage} />
     </Router>
   )
 }
--- web/src/index.js
+++ web/src/index.js
@@ -1,13 +1,17 @@
 import ReactDOM from 'react-dom'
 import { HammerProvider } from '@hammerframework/web'
+import FatalErrorPage from 'src/pages/FatalErrorPage'
+import FatalErrorBoundary from 'src/lib/FatalErrorBoundary'
 
 import Routes from './Routes'
 
 import './index.css'
 
 ReactDOM.render(
-  <HammerProvider>
-    <Routes />
-  </HammerProvider>,
+  <FatalErrorBoundary page={FatalErrorPage}>
+    <HammerProvider>
+      <Routes />
+    </HammerProvider>
+  </FatalErrorBoundary>,
   document.getElementById('hammer-app')
 )
