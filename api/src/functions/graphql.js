2fcab6e131cb46095123737290dd4a158afe573b
Peter Pistorius <peter.pistorius@gmail.com>
2019-12-24 16:01:36 +0200

    Upgrade Prisma again, reset migrations.

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -1,15 +1,9 @@
-import { Photon } from '@generated/photon'
 import { server, makeMergedSchema } from '@hammerframework/api'
 
 import * as todo from 'src/graphql/todo'
 
 const schema = makeMergedSchema([todo])
 
-const photon = new Photon()
-
 export const handler = server({
   schema,
-  context: {
-    photon,
-  },
 }).createHandler()
