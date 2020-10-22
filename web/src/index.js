abeefb969e60b29364ccb671ab4de190d62f7122
Peter Pistorius <peter.pistorius@gmail.com>
2020-10-22 23:10:55 +0200

    Switch Apollo for URQL.

--- web/src/index.js
+++ web/src/index.js
@@ -2,15 +2,21 @@ import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 
+import { createClient, Provider } from 'urql'
+
 import Routes from './Routes'
 
 import './index.css'
 
+const client = createClient({ url: '/api/functions/graphql' })
+
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider>
-      <Routes />
-    </RedwoodProvider>
+    <Provider value={client}>
+      <RedwoodProvider>
+        <Routes />
+      </RedwoodProvider>
+    </Provider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
 )
